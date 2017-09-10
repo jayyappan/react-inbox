3809f3e89cd4fba340e352597c4017a8a0f87a12
JP AYYAPPAN <jayy3@allstate.com>
2017-09-10 10:28:07 -0500

    Load the messages from the server

--- src/App.js
+++ src/App.js
@@ -6,78 +6,17 @@ import Messages from './components/Messages';
 
 class App extends Component {
   constructor (props) {
-    const messages = [
-    {
-      "id": 1,
-      "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
-      "read": false,
-      "starred": true,
-      "labels": ["dev", "personal"],
-      "selected": false
-    },
-    {
-      "id": 2,
-      "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
-      "read": false,
-      "starred": false,
-      "selected": true,
-      "labels": [],
-      "selected": false
-    },
-    {
-      "id": 3,
-      "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
-      "read": false,
-      "starred": true,
-      "labels": ["dev"],
-      "selected": false
-    },
-    {
-      "id": 4,
-      "subject": "We need to program the primary TCP hard drive!",
-      "read": true,
-      "starred": false,
-      "selected": true,
-      "labels": [],
-      "selected": false
-    },
-    {
-      "id": 5,
-      "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
-      "read": false,
-      "starred": false,
-      "labels": ["personal"],
-      "selected": false
-    },
-    {
-      "id": 6,
-      "subject": "We need to back up the wireless GB driver!",
-      "read": true,
-      "starred": true,
-      "labels": [],
-      "selected": false
-    },
-    {
-      "id": 7,
-      "subject": "We need to index the mobile PCI bus!",
-      "read": true,
-      "starred": false,
-      "labels": ["dev", "personal"],
-      "selected": false
-    },
-    {
-      "id": 8,
-      "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
-      "read": true,
-      "starred": true,
-      "labels": [],
-      "selected": false
-    }
-    ]
+    const messages = []
     super(props)
     this.state = {messages : messages}
   }
 
+async componentDidMount() {
+  const response = await fetch("http://localhost:3001/api/messages")
+  const json = await response.json()
+  const messages = json._embedded.messages
+  this.setState({messages: messages})
+}
   selectunselectmessages = (e, select) => {
     let newState = this.state.messages.map((message) => {
     let  newMessage = message
